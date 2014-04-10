5ef5c94a0dbbd53ec6b0cd5559dd299b1946c633
Louie Dinh <ldinh@athinkingape.com>
2014-04-10 14:16:12 -0700

    Add some sample code

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,27 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little tea pot!"))
+
+(defn testing
+  "Just a test function"
+  []
+  (println "Testing Complete!"))
+
+(defn too-enthusiastic
+  "Be waaaay to into someone"
+  [name]
+  (str "OMG YOU'RE the best " name " EVAR AND WE SHOULD RUN AWAY TOGETHER!!!!!@!"))
+
+(defn add
+  "Add n numbers"
+  ([a b] (+ a b))
+  ([a b c] (+ a b c)))
+
+(defn add2
+  [& args]  (eval (conj args '+)))
+
+(defn codger-communication [name]
+  (println (str  "Get off my lawn " name "!!!")))
+
+(defn codger [& names]
+  (map codger-communication names))
