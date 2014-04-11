e6bee693f50692a76820979e63261d6aa5a789f4
louiedinh <louiedinh@gmail.com>
2014-04-11 14:58:25 -0700

    Add hobbit body

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -29,3 +29,53 @@
 
 (defn codger [& names]
   (map codger-communication names))
+
+(defn my-first [[first-thing]] first-thing)
+
+(defn all-the-things [[ first-thing & rest-things]] rest-things)
+
+(defn lat-lng [{lat :lat lng :lng}] (list lat lng))
+
+(defn lat-lng-keys [{:keys [lat lng]}]
+  (println (str  "The treature's at (" lat ", " lng ")")))
+
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn has-matching-part?
+  [part]
+  (re-find #"^left-" (:name part)))
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"left-" "right-") :size (:size part)})
+
+(defn symmetrize
+  [asym-parts]
+  (loop [remaining-parts asym-parts
+         final-parts []]
+    (if (empty? remaining-parts)
+      final-parts
+      (let [[part & remaining] remaining-parts
+            final-parts (conj final-parts part)]
+        (if (has-matching-part? part)
+          (recur remaining (conj final-parts (matching-part part)))
+          (recur remaining final-parts))))))
